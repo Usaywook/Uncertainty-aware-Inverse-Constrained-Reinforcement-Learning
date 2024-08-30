b082e34bb5cee151660590194c0aa1767c0ec96f
usaywook <tpdnr1360@snu.ac.kr>
2024-08-30 12:17:39 +0900

    UAICRL for highD env command added

--- interface/tmp1.sh
+++ interface/tmp1.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-DEVICE=0
-
+DEVICE=6
+SEEDS=(123 321 456)
 # train UAICRL
-CUDA_VISIBLE_DEVICES=$DEVICE python train_icrl.py ../config/Mujoco/Blocked_HalfCheetah/train_UAICRL_HC-noise-1e-1.yaml -n 5 -s 123
-CUDA_VISIBLE_DEVICES=$DEVICE python train_icrl.py ../config/Mujoco/Blocked_HalfCheetah/train_UAICRL_HC-noise-1e-1.yaml -n 5 -s 321
-CUDA_VISIBLE_DEVICES=$DEVICE python train_icrl.py ../config/Mujoco/Blocked_HalfCheetah/train_UAICRL_HC-noise-1e-1.yaml -n 5 -s 456
-CUDA_VISIBLE_DEVICES=$DEVICE python train_icrl.py ../config/Mujoco/Blocked_HalfCheetah/train_UAICRL_HC-noise-1e-1.yaml -n 5 -s 654
-CUDA_VISIBLE_DEVICES=$DEVICE python train_icrl.py ../config/Mujoco/Blocked_HalfCheetah/train_UAICRL_HC-noise-1e-1.yaml -n 5 -s 666
+
+for SEED in ${SEEDS[@]}
+do
+    CUDA_VISIBLE_DEVICES=$DEVICE python train_icrl.py ../config/HighD_velocity_constraint/train_DICRL_QRDQN_CVaR_highD_velocity_constraint.yaml -n 5 -s $SEED
+
+    CUDA_VISIBLE_DEVICES=$DEVICE python train_icrl.py ../config/HighD_velocity_constraint/train_DICRL_QRDQN_EXP_highD_velocity_constraint.yaml -n 5 -s $SEED
+done
+
